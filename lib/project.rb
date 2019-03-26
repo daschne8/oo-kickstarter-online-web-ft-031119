05bb71b03f8f65a64fb5f9def604916686445f39
Daniel Schneider <daschne8@gmail.com>
2019-03-26 12:40:16 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,15 +1,12 @@
 class Project
-  attr_accessor
+  attr_accessor :backers
   attr_reader :title, :backers
   def initialize(title)
     @title = title
     @backers = []
   end
   def add_backer(backer)
-    backer.update(self)
-    @backers << backer
-  end
-  def update(backer)
+    backer.projects << self
     @backers << backer
   end
 
