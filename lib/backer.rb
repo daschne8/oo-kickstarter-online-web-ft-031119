75512593adc035f3bce9c6b389c2b3a094504ea8
Daniel Schneider <daschne8@gmail.com>
2019-03-26 12:40:48 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -6,10 +6,7 @@ class Backer
     @backed_projects = []
   end
   def back_project(project)
-    project.update(self)
-    @backed_projects << project
-  end
-  def update(project)
+    project.backers << self
     @backed_projects << project
   end
 end
